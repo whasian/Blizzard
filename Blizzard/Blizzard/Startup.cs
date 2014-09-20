5c54cda441e159d3473bb2fa2406b378cb43dbe8
whasin <cdone2007@gmail.com>
2014-09-20 16:36:44 -0700

    adding all files

---
+++ Blizzard/Blizzard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blizzard.Startup))]
+namespace Blizzard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
